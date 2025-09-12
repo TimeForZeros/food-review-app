674a6bfc4725583ee16df8f76c02d99fdab32f7d
Victor Gonzalez <dazedbutamused@gmail.com>
2025-09-12 14:53:08 -0700

    init

---
+++ src/app/nav-bar.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import type { ReactNode } from 'react';
+
+type LinkButtonProps = {
+  href: string;
+  children: ReactNode;
+};
+const LinkButton = ({ href, children }: LinkButtonProps) => (
+  <Button asChild className='mx-0.5'>
+    <Link href={href}>{children}</Link>
+  </Button>
+);
+
+const NavBar = () => {
+  return (
+    <header className='flex justify-center'>
+      <nav>
+        <LinkButton href={'/login'}>Log In</LinkButton>
+        <LinkButton href={'/signup'}>Sign Up</LinkButton>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;

32b6ad26371000cb4e49bbac2d9fde1c53dc0a49
Victor Gonzalez <dazedbutamused@gmail.com>
2025-09-13 21:54:10 -0700

    drizzle setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,32 @@
-import { integer, pgTable, varchar, uuid, text } from "drizzle-orm/pg-core";
-import { createSelectSchema } from 'drizzle-zod';
+import { pgTable, varchar, char, uuid, text, check, integer, pgEnum } from 'drizzle-orm/pg-core';
+// import { createSelectSchema, createUpdateSchema, createInsertSchema } from 'drizzle-zod';
 
-const users = pgTable('users', {
+export const users = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
-  name: varchar().notNull(),
-  email: varchar().notNull().unique(),
-  password: 
-})
+  name: varchar({ length: 256 }).notNull(),
+  email: varchar({ length: 320 }).notNull().unique(),
+  password: char({ length: 97 }).notNull(),
+});
+
+export const restaurant = pgTable('restaurant', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: uuid().references(() => users.id),
+  name: varchar({ length: 256 }).notNull(),
+  location: varchar({ length: 512 }),
+  type: char({ enum: ['sit down', 'fast food', 'cafe', 'food truck', 'other'] }),
+  details: text(),
+  rating: char({ enum: ['terrible', 'bad', 'mid', 'good', 'great'] }),
+});
+
+export const item = pgTable('item', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 256 }).notNull(),
+  restaurantId: integer().references(() => restaurant.id),
+  type: char({ enum: ['drink', 'entree', 'combo', 'other'] }),
+  details: text(),
+  rating: char({ enum: ['terrible', 'bad', 'mid', 'good', 'great'] }),
+});
+
+// export const userSelectSchema = createSelectSchema(users);
+// export const userInsertSchema = createInsertSchema(users);
+// export const userUpdateSchema = createUpdateSchema(users);
